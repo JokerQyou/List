fa99fa6149072904252852d0d9aa35b0dac57d3c
Joker Qyou <Joker.Qyou@gmail.com>
2015-07-26 19:53:30 +0800

    Basic form

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # List #
 
 BSD License
+
+# Credits #
+
+* Based on the Vue example of [Todo MVC]( https://github.com/tastejs/todomvc )
+* '[List]( https://thenounproject.com/term/list/57790 )' icon by Bradley Wilto from the [Noun Project]( http://thenounproject.com/ )
--- main.js
+++ main.js
@@ -1,19 +1,48 @@
-var app = require('app');
-var browser = require('browser-window');
+var app = require('app'),
+    path = require('path'),
+    Browser = require('browser-window');
+    Tray = require('tray');
+    Menu = require('menu');
 require('crash-reporter').start();
 
-var mainWindow = null;
+var mainWindow = null,
+    appIcon = null;
 
 app.on('window-all-closed', function(){
-    if(process.platform !== 'darwin'){
-        app.quit();
-    }
+    // if(process.platform !== 'darwin'){
+        // app.quit();
+    // }
 });
 
+var openMainWindow = function(){
+    if(mainWindow === null){
+        mainWindow = new Browser({
+            height: 500,
+            width: 800,
+            frame: false,
+            transparent: true,
+            'standard-window': false
+        });
+        mainWindow.loadUrl('file://' + path.join(__dirname, 'vue', 'index.html'));
+        mainWindow.on('close', function(e){
+            if(mainWindow.isVisible()){
+                mainWindow.hide();
+                return e.preventDefault();
+            }
+        }).on('blur', function(){
+            mainWindow.hide();
+        }).center();
+    }
+};
+
 app.on('ready', function(){
-    mainWindow = new browser({height: 600, width: 800});
-    mainWindow.loadUrl('http://weibo.com');
-    mainWindow.on('closed', function(){
-        mainWindow = null;
-    })
+    appIcon = new Tray(path.join(__dirname, 'icon.png'));
+    appIcon.on('clicked', function(){
+        if(mainWindow){
+            mainWindow.show();
+        }else{
+            openMainWindow();
+        }
+    });
+    openMainWindow();
 });
